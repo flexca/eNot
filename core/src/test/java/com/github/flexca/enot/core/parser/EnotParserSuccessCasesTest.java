7d98080caf9f4d431bfefacabc558c9af3d1c264
FlexCA <flexcainfo@gmail.com>
2026-03-29 13:40:35 +0300

    as model more or less stable now, add assertions for success test.

--- core/src/test/java/com/github/flexca/enot/core/parser/EnotParserSuccessCasesTest.java
+++ core/src/test/java/com/github/flexca/enot/core/parser/EnotParserSuccessCasesTest.java
@@ -1,6 +1,8 @@
 package com.github.flexca.enot.core.parser;
 
 import com.github.flexca.enot.core.asn1.Asn1TypeSpecification;
+import com.github.flexca.enot.core.asn1.attribute.Asn1Attribute;
+import com.github.flexca.enot.core.element.attribute.EnotAttribute;
 import com.github.flexca.enot.core.registry.EnotRegistry;
 import com.github.flexca.enot.core.element.EnotElement;
 import com.github.flexca.enot.core.system.SystemTypeSpecification;
@@ -9,7 +11,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import tools.jackson.databind.ObjectMapper;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -37,6 +41,36 @@ public class EnotParserSuccessCasesTest {
 
         assertThat(actual.size()).isEqualTo(1);
 
+        EnotElement root = actual.get(0);
+        assertThat(root.getType()).isEqualTo(Asn1TypeSpecification.TYPE_NAME);
+        assertThat(root.isOptional()).isFalse();
+        assertThat(root.getAttributes()).isEqualTo(Map.of(Asn1Attribute.TAG, "set"));
+        assertThat(root.getBody() instanceof EnotElement).isTrue();
+
+        EnotElement sequenceElement = (EnotElement) root.getBody();
+        assertThat(sequenceElement.getType()).isEqualTo(Asn1TypeSpecification.TYPE_NAME);
+        assertThat(sequenceElement.isOptional()).isFalse();
+        assertThat(sequenceElement.getAttributes()).isEqualTo(Map.of(Asn1Attribute.TAG, "sequence"));
+        assertThat(sequenceElement.getBody() instanceof List<?>).isTrue();
+
+        List<?> sequenceBody = (List<?>) sequenceElement.getBody();
+        assertThat(sequenceBody).hasSize(2);
+        assertThat(sequenceBody.get(0) instanceof EnotElement);
+        assertThat(sequenceBody.get(1) instanceof EnotElement);
+
+        EnotElement oidElement = (EnotElement) sequenceBody.get(0);
+        assertThat(oidElement.getType()).isEqualTo(Asn1TypeSpecification.TYPE_NAME);
+        assertThat(oidElement.isOptional()).isFalse();
+        assertThat(oidElement.getAttributes()).isEqualTo(Map.of(Asn1Attribute.TAG, "object_identifier"));
+        assertThat(oidElement.getBody() instanceof String);
+        assertThat(oidElement.getBody()).asString().isEqualTo("2.5.4.3");
+
+        EnotElement utf8StringElement = (EnotElement) sequenceBody.get(1);
+        assertThat(utf8StringElement.getType()).isEqualTo(Asn1TypeSpecification.TYPE_NAME);
+        assertThat(utf8StringElement.isOptional()).isFalse();
+        assertThat(utf8StringElement.getAttributes()).isEqualTo(Map.of(Asn1Attribute.TAG, "utf8_string"));
+        assertThat(utf8StringElement.getBody() instanceof String);
+        assertThat(utf8StringElement.getBody()).asString().isEqualTo("${common_name}");
     }
 
     @Test
@@ -85,4 +119,5 @@ public class EnotParserSuccessCasesTest {
         assertThat(actual.size()).isEqualTo(1);
 
     }
+
 }
